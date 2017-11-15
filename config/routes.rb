03ec2def429ee02069f3eb53a131b14f8730b611
Mario Granada <mario.granada@globant.com>
2017-11-15 12:07:40 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
 
+	resources :users
+	
+	resources :games, only: [:create, :show]
+
+	get 'users/:id/games', to: "users#read_games"
+
+	post 'users/:id/games', to: "users#add_game"
+
+	put 'games/:id/', to: 'games#update_grid_status'
+
 end
