8187907cae7d7cf1dcae6ea1a1e8eb846f516482
Mario Granada <mario.granada@globant.com>
2017-11-26 02:00:48 -0500

    update_grid_cells

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,5 @@
 class GamesController < ApplicationController
-	before_action :get_game, only: [:show, :update] 
+	before_action :get_game
 
 	def show
 		json_response(@game)
@@ -8,6 +8,19 @@ class GamesController < ApplicationController
 	def update
 		@game.update(params[:game])
 		head :no_content
+	end
+
+	def update_single_cell
+		position_in_array = params[:array_cell_position]
+		@game.cells[position_in_array] = params[:cell_state]
+		@game.grid_status = GAME_STATUS::isOver if @game.isMineFound(position_in_array)
+		@game.save!
+		json_response(@game)
+	end
+
+	def update_cells_group
+		
+		json_response(@game)
 	end
 
 	private
--- app/models/game.rb
+++ app/models/game.rb
@@ -3,9 +3,10 @@ class Game
   include Mongoid::Timestamps
 
   before_create :init_game
+  before_update :update_grid_when_over
 
   GAME_STATUS = {:isOver => 'OVER', :inGame => 'IN_GAME'}
-  CELL_STATUS = {:red_flag => 'RED_FLAG', :question_mark => 'QUESTION_MARK', :exploded => 'EXPLODED', :covered => 'COVERED'}
+  CELL_STATUS = {:red_flag => 'RED_FLAG', :question_mark => 'QUESTION_MARK', :exploded => 'EXPLODED', :covered => 'COVERED', :uncovered => 'UNCOVERED'}
   
   field :rows, type: Integer
   field :cols, type: Integer
@@ -24,6 +25,10 @@ class Game
   	self.rows * self.cols
   end
 
+  def isMineFound(cell_position)
+  	self.mines_positions.include?(cell_position)
+  end
+
   private
   	def generate_mines_positions
   		self.mines_positions = (0..(self.total_of_cells)).to_a.shuffle.take(self.mines_total)
@@ -38,5 +43,13 @@ class Game
   		end
 
   		self.generate_mines_positions
+  	end
+
+  	def update_grid_when_over
+  		if self.isOver
+  			self.cells.each_with_index do |cell,index|
+  				cell = mines_positions.include?(index) ? CELL_STATUS::exploded : CELL_STATUS::uncovered
+  			end
+  		end
   	end
 end
