50a43d40f0933b1f6b4cbf2fa4889ef274633de2
Mario Granada <mario.granada@globant.com>
2017-12-08 11:53:24 -0500

    headers_for_cors_origin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  after_action :cors_set_access_control_headers
   include Response
+
+  def cors_set_access_control_headers
+  	headers['Access-Control-Allow-Origin'] = '*'
+  	headers['Access-Control-Allow-Methods'] = '*'
+  	headers['Access-Control-Allow-Headers'] = '*'
+  	headers['Access-Control-Max-Age'] = "1728000"
+  end
+
 end
